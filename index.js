2835e216c6969a0b9c3df37d47a93ec1ccbe83c3
micnem00 <michael.nemni@gmail.com>
2024-09-01 17:07:05 +0300

    add dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors'); // Import the cors package
 const app = express();
 const port = 8080;
+require('dotenv').config();
 
 // Middleware to enable CORS
 app.use(cors());
